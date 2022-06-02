3fa0427f8154651b7e36257168873c437f4f4442
DouglasOliveira <douglasoliveiradaluz10@gmail.com>
2022-06-02 12:32:04 -0300

    Add files via upload

---
+++ includes/_dados.php
@@ -0,0 +1,10 @@
+<?php
+
+$produtos = array(
+0 => array('nome'=>'capivara legal','preco'=>'1200.oo','descricao'=>'Descricao capivara legal','imagem'=>'capivaralegal.jpeg')
+1 => array('nome'=>'capivara legal','preco'=>'1200.oo','descricao'=>'Descricao capivara legal','imagem'=>'capivaralegal.jpeg')
+2 => array('nome'=>'capivara legal','preco'=>'1200.oo','descricao'=>'Descricao capivara legal','imagem'=>'capivaralegal.jpeg')
+3 => array('nome'=>'capivara legal','preco'=>'1200.oo','descricao'=>'Descricao capivara legal','imagem'=>'capivaralegal.jpeg')   
+);
+
+ ?>
