d63e3544b197e0916cd29b99e3e175ab5f469320
DouglasOliveira <douglasoliveiradaluz10@gmail.com>
2022-06-09 12:23:18 -0300

    Add files via upload

---
+++ _dados.php
@@ -0,0 +1,29 @@
+<?php
+
+$produtos = array(
+    0 => array('nome'=>'Capivara legal ','preco'=>'2000.00','descricao'=>'capivara legal','imagem'=>'capivaralegal.JPG' ),
+    1 => array('nome'=>'leão sério ','preco'=>'1500.00','descricao'=>'leão sério','imagem'=>'leãosério.jpg' ),
+    2 => array('nome'=>'Cachorro soriso ','preco'=>'3000.00','descricao'=>'Cachorro sorindo','imagem'=>'cachorrosoriso.jpg' ),
+    3 => array('nome'=>'Macaco Filosofo ','preco'=>'1500.00','descricao'=>'Macaco Filosofo','imagem'=>'macacofilosofo.jpg' ),
+    4 => array('nome'=>'Gato Emo ','preco'=>'150.00','descricao'=>'Gato Emo','imagem'=>'gatoemo.jpeg' ),
+    5 => array('nome'=>'Coelho fofo ','preco'=>'3000.00','descricao'=>'Coelho fofo','imagem'=>'coelho.jpg' ),
+    6 => array('nome'=>'O grito','preco'=>'200.00','descricao'=>'O grito','imagem'=>'grito.jpg' ),
+    7 => array('nome'=>'cartinha da obediência ','preco'=>'4000.00','descricao'=>'Cartinha da obediência','imagem'=>'cartinha.jpg' ),
+    8 => array('nome'=>'homem vs mulher ','preco'=>'1340.00','descricao'=>'Dessespero de cada um','imagem'=>'desespero.jpg' ),
+    9 => array('nome'=>'Homens ','preco'=>'100.00','descricao'=>'O pensamento do homem','imagem'=>'homens.jpg' ),
+    10 => array('nome'=>'mulher ','preco'=>'300.00','descricao'=>'Mulher no volante','imagem'=>'mulher.jpg' ),
+    11 => array('nome'=>'mulher ','preco'=>'300.00','descricao'=>'Mulher no volante','imagem'=>'mulher.jpg' ),
+    12 => array('nome'=>'mulher ','preco'=>'300.00','descricao'=>'Mulher no volante','imagem'=>'mulher.jpg' ),
+    13 => array('nome'=>'mulher ','preco'=>'300.00','descricao'=>'Mulher no volante','imagem'=>'mulher.jpg' )
+    14 => array('nome'=>'mulher ','preco'=>'300.00','descricao'=>'Mulher no volante','imagem'=>'mulher.jpg' )
+    15 => array('nome'=>'mulher ','preco'=>'300.00','descricao'=>'Mulher no volante','imagem'=>'mulher.jpg' )
+    16 => array('nome'=>'mulher ','preco'=>'300.00','descricao'=>'Mulher no volante','imagem'=>'mulher.jpg' )
+    17 => array('nome'=>'mulher ','preco'=>'300.00','descricao'=>'Mulher no volante','imagem'=>'mulher.jpg' )
+    18 => array('nome'=>'mulher ','preco'=>'300.00','descricao'=>'Mulher no volante','imagem'=>'mulher.jpg' )
+    19 => array('nome'=>'mulher ','preco'=>'300.00','descricao'=>'Mulher no volante','imagem'=>'mulher.jpg' )
+    20 => array('nome'=>'mulher ','preco'=>'300.00','descricao'=>'Mulher no volante','imagem'=>'mulher.jpg' )
+    22 => array('nome'=>'mulher ','preco'=>'300.00','descricao'=>'Mulher no volante','imagem'=>'mulher.jpg' )
+    23 => array('nome'=>'mulher ','preco'=>'300.00','descricao'=>'Mulher no volante','imagem'=>'mulher.jpg' )
+);
+
+?>
