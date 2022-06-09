3f8973f4764985c608e65f84d3fdd4574baba819
DouglasOliveira <douglasoliveiradaluz10@gmail.com>
2022-06-09 14:45:18 -0300

    Add files via upload

--- includes/_dados.php
+++ includes/_dados.php
@@ -1,17 +1,37 @@
 <?php
 
 $produtos = array(
-    0 => array('nome'=>'Capivara legal ','preco'=>'2000.00','descricao'=>'capivara legal','imagem'=>'capivaralegal.JPG' )
-    1 => array('nome'=>'leão sério ','preco'=>'1500.00','descricao'=>'leão sério','imagem'=>'leãosério.jpg' )
-    2 => array('nome'=>'Cachorro soriso ','preco'=>'3000.00','descricao'=>'Cachorro sorindo','imagem'=>'cachorrosoriso.jpg' )
-    3 => array('nome'=>'Macaco Filosofo ','preco'=>'1500.00','descricao'=>'Macaco Filosofo','imagem'=>'macacofilosofo.jpg' )
-    4 => array('nome'=>'Gato Emo ','preco'=>'150.00','descricao'=>'Gato Emo','imagem'=>'gatoemo.jpeg' )
-    5 => array('nome'=>'Coelho fofo ','preco'=>'3000.00','descricao'=>'Coelho fofo','imagem'=>'coelho.jpg' )
-    6 => array('nome'=>'O grito','preco'=>'200.00','descricao'=>'O grito','imagem'=>'grito.jpg' )
-    7 => array('nome'=>'cartinha da obediência ','preco'=>'4000.00','descricao'=>'Cartinha da obediência','imagem'=>'cartinha.jpg' )
-    8 => array('nome'=>'homem vs mulher ','preco'=>'1340.00','descricao'=>'Dessespero de cada um','imagem'=>'desespero.jpg' )
-    9 => array('nome'=>'Homens ','preco'=>'100.00','descricao'=>'O pensamento do homem','imagem'=>'homens.jpg' )
-    10 => array('nome'=>'mulher ','preco'=>'300.00','descricao'=>'Mulher no volante','imagem'=>'mulher.jpg' )
+    
+    0 =>  array('nome'=>'Capivara legal ','preco'=>'R$2000.00','descricao'=>'capivara legal','imagem'=>'capivaralegal.JPG' ),
+    1 =>  array('nome'=>'leão sério ','preco'=>'R$1500.00','descricao'=>'leão sério','imagem'=>'leãosério.jpg' ),
+    2 =>  array('nome'=>'Cachorro soriso ','preco'=>'R$3000.00','descricao'=>'Cachorro sorindo','imagem'=>'cachorrosoriso.jpg' ),
+    3 =>  array('nome'=>'Macaco Filosofo ','preco'=>'R$1500.00','descricao'=>'Macaco Filosofo','imagem'=>'macacofilosofo.jpg' ),
+    4 =>  array('nome'=>'Gato Emo ','preco'=>'R$150.00','descricao'=>'Gato Emo','imagem'=>'gatoemo.jpeg' ),
+    5 =>  array('nome'=>'Coelho fofo ','preco'=>'R$3000.00','descricao'=>'Coelho fofo','imagem'=>'coelho.jpg' ),
+    6 =>  array('nome'=>'O grito','preco'=>'R$200.00','descricao'=>'O grito','imagem'=>'grito.jpg' ),
+    7 =>  array('nome'=>'cartinha da obediência ','preco'=>'R$4000.00','descricao'=>'Cartinha da obediência','imagem'=>'cartinha.jpg' ),
+    8 =>  array('nome'=>'homem vs mulher ','preco'=>'R$1340.00','descricao'=>'Dessespero de cada um','imagem'=>'desespero.jpg' ),
+    9 =>  array('nome'=>'Homens ','preco'=>'R$100.00','descricao'=>'O pensamento do homem','imagem'=>'homens.jpg' ),
+    10 => array('nome'=>'mulher ','preco'=>'R$300.00','descricao'=>'Mulher no volante','imagem'=>'mulher.jpg' ),
+    11 => array('nome'=>'presidente ','preco'=>'R$20.00','descricao'=>'palmeiras','imagem'=>'palmerense.jpg' ),
+    12 => array('nome'=>'Criancas ','preco'=>'R$300.00','descricao'=>'gerações','imagem'=>'crianças.jpg' ),
+    13 => array('nome'=>'Ostentando a riqueza ','preco'=>'R$2000.00','descricao'=>'como se sentir ganhando na loteria','imagem'=>'ostentação.jpg' ),
+    14 => array('nome'=>'vingança ','preco'=>'R$90.00','descricao'=>'quem gosta do frio','imagem'=>'cachorro.jpg' ),
+    15 => array('nome'=>'Hipocrisia ','preco'=>'R$1200.00','descricao'=>'hipocrisia vegana','imagem'=>'hipocrisia.jpg' ),
+    16 => array('nome'=>'Investindo ','preco'=>'R$12.00','descricao'=>'investindo no marketing','imagem'=>'investir.jpg' ),
+    17 => array('nome'=>'Piada ','preco'=>'R$700.00','descricao'=>'comédia de ouro','imagem'=>'piada.jpg' ),
+    18 => array('nome'=>'Maquiagem ','preco'=>'R$800.00','descricao'=>'teste a maquiagem','imagem'=>'maquiagem.jpg' ),
+    19 => array('nome'=>'Gato triste ','preco'=>'R$10.90','descricao'=>'gato triste','imagem'=>'triste.jpg' ),
+    20 => array('nome'=>' beleza','preco'=>'R$1.23','descricao'=>'como pensão e como realmente sou','imagem'=>'beleza.jpg' ),
+    22 => array('nome'=>'gasolina ','preco'=>'R$960.90','descricao'=>'não adianta','imagem'=>'gsolina.jpeg' ),
+    23 => array('nome'=>'Sad music ','preco'=>'R$814.00','descricao'=>'quando quer sofrer','imagem'=>'sad.jpg' ),
+    24 => array('nome'=>' arte','preco'=>'R$50.00','descricao'=>'comtmplar','imagem'=>'arte.jpg' ),
+    25 => array('nome'=>'servir ','preco'=>'R$1234.00','descricao'=>'mulher servido','imagem'=>'servir.jpg' ),
+    26 => array('nome'=>' sacanagem','preco'=>'R$100.00','descricao'=>'até as artes falando para pagar o boleto','imagem'=>'sacanagem.jpg' ),
+    27 => array('nome'=>'vacina ','preco'=>'R$600.00','descricao'=>'cadê?','imagem'=>'vacina.jpg' ),
+    28 => array('nome'=>'Mona Lisa ','preco'=>'R$350.00','descricao'=>'lição de moral','imagem'=>'monalisa.jpg' ),
+    29 => array('nome'=>'sapato ','preco'=>'R$730.00','descricao'=>'irrita','imagem'=>'sapato.jpg' ),
+    30 => array('nome'=>'Reflexo ','preco'=>'R$674.00','descricao'=>'Mulher no volante','imagem'=>'auto estima.jpg' ),
 );
 
 ?>
